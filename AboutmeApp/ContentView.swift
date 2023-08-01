7c2be38f2cfa8405e3b03cc35e3bcf71e4777c99
Laura Weber Bernardo <laurawb6@gmail.com>
2023-08-01 09:31:27 +0100

    added first comment

--- AboutmeApp/ContentView.swift
+++ AboutmeApp/ContentView.swift
@@ -6,18 +6,60 @@
 //
 
 import SwiftUI
+//Comment
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemGray)
+                .ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 20.0) {
+                
+                Image("laura")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .contentShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(20)
+                
+                HStack(spacing: 20.0){
+                    Text("Laura Weber Bernardo")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .multilineTextAlignment(.center)
+                    
+                    
+
+                }
+                
+                Text("Heyy, I am a regular 17 year old girl that lives in Germany. ")
+                Text("One of my interests is golf. It has been my hobby for 6 years now. The course that I play in is called Golfclub Neckartal. You can see the logo below.")
+                    .font(.caption)
+                    .fontWeight(.regular)
+                    .foregroundColor(Color.black)
+                Image("lauragolf")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .contentShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(20)
+                Image("golfclubneckartal")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .contentShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(80)
+
+                        }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white))
+            .cornerRadius(30)
+            .shadow(radius: 50)
+                    .padding()
         }
-        .padding()
+  
+
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
